d9d3810fb093bb706d5faa8c21721e0f2c6665ca
11gather11 <11gather11@gmail.com>
2025-09-30 16:30:43 +0900

    feat: ホームコンポーネントにバージョン情報を追加

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
+import version from '../../package.json'
 
 export const Route = createFileRoute('/')({
 	component: Home,
 })
 
 function Home() {
-	return <h1>Hello</h1>
+	return <h1>Hello World! v {version.version}</h1>
 }
